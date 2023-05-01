6fac8c1c87ce5a0173693d3175cfc50074d42afd
mafzalcode <126709582+mafzalcode@users.noreply.github.com>
2023-05-01 17:56:58 +0500

    Update Repo.tsx

--- src/Repo.tsx
+++ src/Repo.tsx
@@ -18,12 +18,12 @@ const Repo = () => {
   const handleSearch = async () => {
     const accessToken = import.meta.env.GITHUB_ACCESS_TOKEN;
     const response = await fetch(
-      `https://api.github.com/repos/${authorName}/${repoName}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
+      `https://api.github.com/repos/${authorName}/${repoName}`
+      //{
+      //  headers: {
+      //    Authorization: `Bearer ${accessToken}`,
+      //  },
+      //}
     );
     if (response.ok) {
       const data = await response.json();
